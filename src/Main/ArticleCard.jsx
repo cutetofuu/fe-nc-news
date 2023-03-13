d5f26b4b5cda728a527b7606e4f327f7ca00a31a
Kate Dollete <cfdollete@up.edu.ph>
2023-03-13 19:33:46 +0000

    Add ArticleCard formatting and styling

--- src/Main/ArticleCard.jsx
+++ src/Main/ArticleCard.jsx
@@ -7,20 +7,24 @@ export const ArticleCard = ({
   topic,
   created_at,
 }) => {
+  const date = new Date(created_at);
   return (
     <section className="section__article">
-      <img src={article_img_url} alt="article image" className="img__article" />
+      <img src={article_img_url} alt={title} className="article__img" />
       <h2 className="article__title">{title}</h2>
-      <p>By: {author}</p>
-      <span>
-        <i class="fa-regular fa-thumbs-up"></i> {votes}
-      </span>
-      <span>
-        <i class="fa-regular fa-comment"></i> {comment_count}
-      </span>
+      <p className="article__author_and_date">
+        By {author} on {date.toLocaleDateString()}
+      </p>
       <div className="article__card__footer">
-        <p>{topic}</p>
-        <p>{created_at}</p>
+        <span>#{topic}</span>
+        <div className="article__icons">
+          <span>
+            <i className="fa-regular fa-thumbs-up"></i> {votes}
+          </span>
+          <span>
+            <i className="fa-regular fa-comment"></i> {comment_count}
+          </span>
+        </div>
       </div>
     </section>
   );
