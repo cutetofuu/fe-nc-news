046410836933e5203d3f8356f08d6dab9f324932
Kate Dollete <cfdollete@up.edu.ph>
2023-03-14 13:51:48 +0000

    Separate article API get requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,16 +4,14 @@ const newsApi = axios.create({
   baseURL: "https://nc-news-2yza.onrender.com/api",
 });
 
-export const getArticles = (article_id) => {
-  let path = `/articles`;
-  if (article_id) {
-    path += `/${article_id}`;
-  }
+export const getArticles = () => {
+  return newsApi.get("/articles").then(({ data }) => {
+    return data.articles;
+  });
+};
 
-  return newsApi.get(path).then(({ data }) => {
-    if (data.hasOwnProperty("articles")) {
-      return data.articles;
-    }
+export const getArticleById = (article_id) => {
+  return newsApi.get(`articles/${article_id}`).then(({ data }) => {
     return data.article;
   });
 };
