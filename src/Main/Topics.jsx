55ea45895d784c8d6e89ada0d354ba0193cb32b1
Kate Dollete <cfdollete@up.edu.ph>
2023-03-16 12:12:38 +0000

    Add states and loading pattern

--- src/Main/Topics.jsx
+++ src/Main/Topics.jsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from "react";
+import { getTopics } from "../utils/api";
+
 export const Topics = () => {
-  return <p>Hellooooo topics!</p>;
+  const [topics, setTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTopics().then((topicsData) => {
+      setTopics(topicsData);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    <main>
+      {isLoading ? (
+        <p>Loading topics...</p>
+      ) : (
+        <>
+          <h2>Select a topic</h2>
+          <ul>
+            {topics.map((topic) => {
+              return (
+                <li key={topic.slug}>
+                  <p>{topic.slug}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
+    </main>
+  );
 };
