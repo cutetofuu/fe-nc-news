d5dfb56dd9aa952ee301029aa401bcaad6c2d785
Kate Dollete <cfdollete@up.edu.ph>
2023-03-16 13:12:57 +0000

    Add topic buttons and styling

--- src/Main/Topics.jsx
+++ src/Main/Topics.jsx
@@ -3,6 +3,7 @@ import { getTopics } from "../utils/api";
 
 export const Topics = () => {
   const [topics, setTopics] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -18,18 +19,24 @@ export const Topics = () => {
       {isLoading ? (
         <p>Loading topics...</p>
       ) : (
-        <>
+        <section className="section__topics">
           <h2>Select a topic</h2>
           <ul>
             {topics.map((topic) => {
               return (
                 <li key={topic.slug}>
-                  <p>{topic.slug}</p>
+                  <button
+                    className="button__topic"
+                    value={topic.slug}
+                    onClick={() => setSelectedTopic(topic.slug)}
+                  >
+                    {topic.slug}
+                  </button>
                 </li>
               );
             })}
           </ul>
-        </>
+        </section>
       )}
     </main>
   );
