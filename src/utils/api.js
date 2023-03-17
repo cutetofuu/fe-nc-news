a6fd8234697e6d70f62918d9852d721e5f8c3df2
Kate Dollete <cfdollete@up.edu.ph>
2023-03-17 08:24:12 +0000

    Add deleteComment function

--- src/utils/api.js
+++ src/utils/api.js
@@ -40,3 +40,7 @@ export const postComment = (article_id, newComment, loggedInUser) => {
       return data.comment;
     });
 };
+
+export const deleteComment = (comment_id) => {
+  return newsApi.delete(`/comments/${comment_id}`);
+};
