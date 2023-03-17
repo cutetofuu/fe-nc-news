5a6c59796f03384de483312331d0d12af49a165a
Kate Dollete <cfdollete@up.edu.ph>
2023-03-17 08:22:46 +0000

    Fix syntax error

--- src/Main/CommentAdder.jsx
+++ src/Main/CommentAdder.jsx
@@ -36,7 +36,7 @@ export const CommentAdder = ({ article_id, setComments, loggedInUser }) => {
 
   return (
     <form className="comment_adder" onSubmit={handleSubmit}>
-      <label htmlFor="new_comment" class="new_comment">
+      <label htmlFor="new_comment" className="new_comment">
         <h3>Add a Comment</h3>
         <textarea
           id="new_comment"
