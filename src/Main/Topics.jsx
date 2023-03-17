6f5eb654fef1ca1250d62f3a35edc4c6fff9eaba
Kate Dollete <cfdollete@up.edu.ph>
2023-03-17 13:16:50 +0000

    Remove button element as per feedback

--- src/Main/Topics.jsx
+++ src/Main/Topics.jsx
@@ -20,15 +20,13 @@ export const Topics = () => {
         <p>Loading topics...</p>
       ) : (
         <section className="section__topics">
-          <h2>Select a topic</h2>
-          <ul>
+          <h2 className="header__topic">Select a topic</h2>
+          <ul className="list__topic">
             {topics.map((topic) => {
               return (
-                <li key={topic.slug}>
-                  <Link to={`/topics/${topic.slug}`}>
-                    <button className="button__topic" value={topic.slug}>
-                      {topic.slug}
-                    </button>
+                <li key={topic.slug} className="li__topic">
+                  <Link to={`/topics/${topic.slug}`} className="link__topic">
+                    {topic.slug}
                   </Link>
                 </li>
               );
